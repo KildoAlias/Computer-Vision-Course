2e17e877919ac2071ac40375c9fb68d7f2991e92
KildoAlias <kildo.alias@live.com>
2019-12-04 18:01:05 +0100

    modified files

--- Lab3/bildat_lab3/graphcut_example.m
+++ Lab3/bildat_lab3/graphcut_example.m
@@ -1,5 +1,5 @@
 scale_factor = 0.5;          % image downscale factor
-area = [ 80, 110, 570, 300 ] % image region to train foreground with
+area = [80, 110, 570, 300 ] % image region to train foreground with
 K = 16;                      % number of mixture components
 alpha = 8.0;                 % maximum edge cost
 sigma = 10.0;                % edge cost decay factor
@@ -7,6 +7,7 @@ sigma = 10.0;                % edge cost decay factor
 I = imread('tiger1.jpg');
 I = imresize(I, scale_factor);
 Iback = I;
+
 area = int16(area*scale_factor);
 [ segm, prior ] = graphcut_segm(I, area, K, alpha, sigma);
 
--- Lab3/bildat_lab3/norm_cuts_example.m
+++ Lab3/bildat_lab3/norm_cuts_example.m
@@ -1,4 +1,4 @@
-close all
+%close all
 figure()
 colour_bandwidth = 20.0; % color bandwidth
 radius = 3;              % maximum neighbourhood distance
@@ -10,8 +10,8 @@ image_sigma = 2.0;       % image preblurring scale
 
 I = imread('tiger3.jpg');
 I = imresize(I, scale_factor);
-subplot(1,3,1)
-imshow(I)
+% subplot(1,3,1)
+% imshow(I)
 
 Iback = I;
 d = 2*ceil(image_sigma*2) + 1;
@@ -21,11 +21,12 @@ I = imfilter(I, h);
 segm = norm_cuts_segm(I, colour_bandwidth, radius, ncuts_thresh, min_area, max_depth);
 Inew = mean_segments(Iback, segm);
 I = overlay_bounds(Iback, segm);
-
-subplot(1,3,2)
 imshow(I)
-subplot(1,3,3)
-imshow(Inew)
+
+% subplot(1,3,2)
+% imshow(I)
+% subplot(1,3,3)
+% imshow(Inew)
 % imwrite(Inew,'result/normcuts1.png')
 % imwrite(I,'result/normcuts2.png')
 
